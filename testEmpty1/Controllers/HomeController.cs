5728c22fa9fbe34db649e43054c43d480d35a571
baby7305 <songhaibei7305@163.com>
2018-04-10 13:50:02 +0800

    Controller-C#代码嵌入View-cshtml页面测试

---
+++ testEmpty1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testEmpty1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
