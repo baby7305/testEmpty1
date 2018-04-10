3210976fca71f7e78768ddd53cec33b8bd268ffc
baby7305 <songhaibei7305@163.com>
2018-04-10 11:53:40 +0800

    console硬编码进来

--- testEmpty1/Startup.cs
+++ testEmpty1/Startup.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using testEmpty1.Models;
 
 namespace testEmpty1
 {
     public class Startup
     {
+        private static readonly HttpClient client = new HttpClient();
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,8 +32,35 @@ namespace testEmpty1
 
             app.Run(async (context) =>
             {
+                var repositories = ProcessRepositories().Result;
+
+                foreach (var repo in repositories)
+                {
+                    Console.WriteLine(repo.Name);
+                    //Console.WriteLine(repo.Description);
+                    //Console.WriteLine(repo.GitHubHomeUrl);
+                    //Console.WriteLine(repo.Homepage);
+                    //Console.WriteLine(repo.Watchers);
+                    //Console.WriteLine(repo.LastPush);
+                    Console.WriteLine();
+                }
                 await context.Response.WriteAsync("Hello World!");
             });
+        }
+        
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            return repositories;
         }
     }
 }
