217829e1838dae61489b6545cae1bf70bf420a56
baby7305 <songhaibei7305@163.com>
2018-04-10 15:15:16 +0800

    异步方法从restful api拉到数据，@Model依赖注入页面展示，只能单个对象，内存List取对象用了Linq。@HttpClient依赖注入失败、未初始化，getJsonAsync方法不支持，不能智能json转对象

--- testEmpty1/Controllers/HomeController.cs
+++ testEmpty1/Controllers/HomeController.cs
@@ -1,14 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using testEmpty1.Models;
 
 namespace testEmpty1.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly HttpClient client = new HttpClient();
+
         // GET
         public IActionResult Index()
         {
             return
-            View();
+                View();
+        }
+
+        public IActionResult LoadData()
+        {
+            var repositories = ProcessRepositories().Result;
+            var repo1 = repositories.First();
+            return View(repo1);
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+
+            return repositories;
         }
     }
 }
